9c6a442c1aa2a1512255fc0481b7c7000c35f45e
etharani <tharani2297@gmail.com>
2024-02-06 15:37:23 +0530

    Calculator user interface is complete.

---
+++ Calculator/Calculator/Form1.Designer.cs
@@ -0,0 +1,239 @@
+﻿namespace Calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.sum = new System.Windows.Forms.Button();
+            this.output = new System.Windows.Forms.Label();
+            this.num2 = new System.Windows.Forms.Label();
+            this.num1 = new System.Windows.Forms.Label();
+            this.txtoutput = new System.Windows.Forms.TextBox();
+            this.txtnum2 = new System.Windows.Forms.TextBox();
+            this.txtnum1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.sub = new System.Windows.Forms.Button();
+            this.mul = new System.Windows.Forms.Button();
+            this.div = new System.Windows.Forms.Button();
+            this.rem = new System.Windows.Forms.Button();
+            this.clear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // sum
+            // 
+            this.sum.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.sum.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sum.Location = new System.Drawing.Point(65, 229);
+            this.sum.Name = "sum";
+            this.sum.Size = new System.Drawing.Size(39, 41);
+            this.sum.TabIndex = 15;
+            this.sum.Text = "+";
+            this.sum.UseVisualStyleBackColor = false;
+            // 
+            // output
+            // 
+            this.output.AutoSize = true;
+            this.output.Font = new System.Drawing.Font("Microsoft PhagsPa", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.output.Location = new System.Drawing.Point(16, 180);
+            this.output.Name = "output";
+            this.output.Size = new System.Drawing.Size(84, 26);
+            this.output.TabIndex = 14;
+            this.output.Text = "Output :";
+            // 
+            // num2
+            // 
+            this.num2.AutoSize = true;
+            this.num2.Font = new System.Drawing.Font("Microsoft PhagsPa", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.num2.Location = new System.Drawing.Point(16, 130);
+            this.num2.Name = "num2";
+            this.num2.Size = new System.Drawing.Size(109, 26);
+            this.num2.TabIndex = 13;
+            this.num2.Text = "Number 2 :";
+            // 
+            // num1
+            // 
+            this.num1.AutoSize = true;
+            this.num1.Font = new System.Drawing.Font("Microsoft PhagsPa", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.num1.Location = new System.Drawing.Point(16, 80);
+            this.num1.Name = "num1";
+            this.num1.Size = new System.Drawing.Size(109, 26);
+            this.num1.TabIndex = 12;
+            this.num1.Text = "Number 1 :";
+            // 
+            // txtoutput
+            // 
+            this.txtoutput.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtoutput.Location = new System.Drawing.Point(157, 176);
+            this.txtoutput.Name = "txtoutput";
+            this.txtoutput.Size = new System.Drawing.Size(247, 28);
+            this.txtoutput.TabIndex = 11;
+            // 
+            // txtnum2
+            // 
+            this.txtnum2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtnum2.Location = new System.Drawing.Point(157, 126);
+            this.txtnum2.Name = "txtnum2";
+            this.txtnum2.Size = new System.Drawing.Size(247, 28);
+            this.txtnum2.TabIndex = 10;
+            // 
+            // txtnum1
+            // 
+            this.txtnum1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtnum1.Location = new System.Drawing.Point(157, 76);
+            this.txtnum1.Name = "txtnum1";
+            this.txtnum1.Size = new System.Drawing.Size(247, 28);
+            this.txtnum1.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.AllowDrop = true;
+            this.label1.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label1.Font = new System.Drawing.Font("Miriam CLM", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(416, 61);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Calculator";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AllowDrop = true;
+            this.label2.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.label2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.label2.Font = new System.Drawing.Font("Miriam CLM", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label2.Location = new System.Drawing.Point(0, 286);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(416, 28);
+            this.label2.TabIndex = 16;
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // sub
+            // 
+            this.sub.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.sub.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sub.Location = new System.Drawing.Point(110, 229);
+            this.sub.Name = "sub";
+            this.sub.Size = new System.Drawing.Size(39, 41);
+            this.sub.TabIndex = 17;
+            this.sub.Text = "-";
+            this.sub.UseVisualStyleBackColor = false;
+            // 
+            // mul
+            // 
+            this.mul.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.mul.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.mul.Location = new System.Drawing.Point(155, 229);
+            this.mul.Name = "mul";
+            this.mul.Size = new System.Drawing.Size(39, 41);
+            this.mul.TabIndex = 18;
+            this.mul.Text = "*";
+            this.mul.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.mul.UseVisualStyleBackColor = false;
+            // 
+            // div
+            // 
+            this.div.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.div.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.div.Location = new System.Drawing.Point(200, 229);
+            this.div.Name = "div";
+            this.div.Size = new System.Drawing.Size(39, 41);
+            this.div.TabIndex = 19;
+            this.div.Text = "/";
+            this.div.UseVisualStyleBackColor = false;
+            // 
+            // rem
+            // 
+            this.rem.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.rem.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rem.Location = new System.Drawing.Point(245, 229);
+            this.rem.Name = "rem";
+            this.rem.Size = new System.Drawing.Size(39, 41);
+            this.rem.TabIndex = 20;
+            this.rem.Text = "%";
+            this.rem.UseVisualStyleBackColor = false;
+            // 
+            // clear
+            // 
+            this.clear.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.clear.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.clear.Location = new System.Drawing.Point(302, 229);
+            this.clear.Name = "clear";
+            this.clear.Size = new System.Drawing.Size(102, 41);
+            this.clear.TabIndex = 21;
+            this.clear.Text = "Clear";
+            this.clear.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.ClientSize = new System.Drawing.Size(416, 314);
+            this.Controls.Add(this.clear);
+            this.Controls.Add(this.rem);
+            this.Controls.Add(this.div);
+            this.Controls.Add(this.mul);
+            this.Controls.Add(this.sub);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.sum);
+            this.Controls.Add(this.output);
+            this.Controls.Add(this.num2);
+            this.Controls.Add(this.num1);
+            this.Controls.Add(this.txtoutput);
+            this.Controls.Add(this.txtnum2);
+            this.Controls.Add(this.txtnum1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button sum;
+        private System.Windows.Forms.Label output;
+        private System.Windows.Forms.Label num2;
+        private System.Windows.Forms.Label num1;
+        private System.Windows.Forms.TextBox txtoutput;
+        private System.Windows.Forms.TextBox txtnum2;
+        private System.Windows.Forms.TextBox txtnum1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button sub;
+        private System.Windows.Forms.Button mul;
+        private System.Windows.Forms.Button div;
+        private System.Windows.Forms.Button rem;
+        private System.Windows.Forms.Button clear;
+    }
+}
+
